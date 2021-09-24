c6a4459db4bdabe21295e0e5225cd2fd6307b086
Judson Paiva <judson.paiva@hotmail.com>
2021-09-24 12:54:26 +0100

    finalizando App

--- app/src/main/java/com/judson/componente_seekbar/MainActivity.kt
+++ app/src/main/java/com/judson/componente_seekbar/MainActivity.kt
@@ -2,10 +2,33 @@ package com.judson.componente_seekbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var contadora: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBar_.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                txt_valorSeek.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                Toast.makeText(applicationContext, "Começou!!"+seekBar_.progress, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Toast.makeText(applicationContext, "Terminou!!", Toast.LENGTH_LONG).show()
+            }
+        })
+
+
+
     }
 }
